6e2f1d45eb5022688b153cde38dd19f8fe0e03e5
Semen Bevzuk <semen.bevzuk@gmail.com>
2019-10-13 19:00:46 +0300

    Add b1, b2

--- my_net.py
+++ my_net.py
@@ -5,21 +5,22 @@ from datetime import datetime
 
 
 class NeuralNetwork:
-    def __init__(self, nodes_hidden=30, nodes_output=10, lr_hidden=0.4, lr_output=0.1):
+    def __init__(self, nodes_hidden=30, nodes_output=10, lr=0.1):
         self._layers_size = [0, nodes_hidden, nodes_output]
-        self._lr_hidden = lr_hidden
-        self._lr_output = lr_output
+        self._lr = lr
         self._W1 = np.array([])
         self._W2 = np.array([])
+        self._b1 = np.array([])
+        self._b2 = np.array([])
         self._n = 0
         self._batch_size = 0
 
     def forward(self, X):
         X = X.T
-        WX = self._W1.dot(X)
+        WX = self._W1.dot(X) + self._b1
         X = self._ReLU(WX)
 
-        WX = self._W2.dot(X)
+        WX = self._W2.dot(X) + self._b2
         X = self._Softmax(WX)
 
         return X
@@ -39,39 +40,42 @@ class NeuralNetwork:
         self._batch_size = batch_size
         self._n = X.shape[0]
         self._layers_size[0] = X.shape[1]
-        self._initialize_parameters()
+        self._initialize_weights()
 
         epoch_percent = 0
         for epoch in range(number_epochs):
             X, Y = self._shuffle_arrays_together(X, Y)
             for i in range(0, self._n, self._batch_size):
                 U, storage = self._fit_forward(X[i:i + self._batch_size])
-                dW1, dW2 = self._calculate_derivatives(X[i:i + self._batch_size], Y[i:i + self._batch_size], storage)
-                self._W1 = self._W1 + self._lr_hidden * dW1
-                self._W2 = self._W2 + self._lr_output * dW2
+                dW1, dW2, db1, db2 = self._calculate_derivatives(X[i:i + self._batch_size], Y[i:i + self._batch_size], storage)
+                self._W1 = self._W1 + self._lr * dW1
+                self._W2 = self._W2 + self._lr * dW2
+                self._b1 = self._b1 + self._lr * db1
+                self._b2 = self._b2 + self._lr * db2
 
             if epoch % round(number_epochs / 10) == 0:
                 epoch_percent += 10
-                accuracy, crossentropy = self.predict(X, Y)
+                crossentropy, accuracy = self.predict(X, Y)
                 print('{}% Train Accuracy: {}; Train loss: {}'.format(epoch_percent, accuracy, crossentropy))
 
-    def _initialize_parameters(self):
-        sigma_1 = 2. / np.sqrt(self._layers_size[0]+self._layers_size[1])
+    def _initialize_weights(self):
+        sigma_1 = 2. / np.sqrt(self._layers_size[0] + self._layers_size[1])
         sigma_2 = 2. / np.sqrt(self._layers_size[1] + self._layers_size[2])
         self._W1 = sigma_1 * np.random.randn(self._layers_size[1], self._layers_size[0])
         self._W2 = sigma_2 * np.random.randn(self._layers_size[2], self._layers_size[1])
-
+        self._b1 = np.zeros((self._layers_size[1], 1))
+        self._b2 = np.zeros((self._layers_size[2], 1))
 
     def _fit_forward(self, X):
         storage = {}
 
         X = X.T
-        WX = self._W1.dot(X)
+        WX = self._W1.dot(X) + self._b1
         X = self._ReLU(WX)
         storage['X1'] = X
         storage['WX1'] = WX
 
-        WX = self._W2.dot(X)
+        WX = self._W2.dot(X) + self._b2
         X = self._Softmax(WX)
         storage['X2'] = X
         storage['W2'] = self._W2
@@ -81,11 +85,13 @@ class NeuralNetwork:
     def _calculate_derivatives(self, X0, Y, storage):
         delta_2 = Y.T - storage['X2']  # Y - U
         dW2 = delta_2.dot(storage['X1'].T) / self._batch_size
+        db2 = np.sum(delta_2, axis=1, keepdims=True) / self._batch_size
 
         delta_1 = storage['W2'].T.dot(delta_2) * self._ReLU_derivative(storage['WX1'])
         dW1 = delta_1.dot(X0) / self._batch_size
+        db1 = np.sum(delta_1, axis=1, keepdims=True) / self._batch_size
 
-        return dW1, dW2
+        return dW1, dW2, db1, db2
 
     @staticmethod
     def _ReLU(X):
@@ -109,12 +115,12 @@ class NeuralNetwork:
         return a, b
 
 
-def fit_and_test_net_on_MNIST(hidden_size=30, batch_size=128, num_epochs=20, lr_hidden=0.1, lr_output=0.1):
+def fit_and_test_net_on_MNIST(hidden_size=30, batch_size=128, num_epochs=20, lr=0.1):
     (train_x, train_y), (test_x, test_y) = read_mnist.get_MNIST_Keras()
     train_x, test_x = read_mnist.normalized_MNIST(train_x, test_x)
 
     time_start = datetime.now()
-    net = NeuralNetwork(nodes_hidden=hidden_size, lr_hidden=lr_hidden, lr_output=lr_output)
+    net = NeuralNetwork(nodes_hidden=hidden_size, lr=lr)
     net.fit(train_x, train_y, batch_size=batch_size, number_epochs=num_epochs)
 
     delta_time = datetime.now() - time_start
@@ -124,7 +130,7 @@ def fit_and_test_net_on_MNIST(hidden_size=30, batch_size=128, num_epochs=20, lr_
 
 
 if __name__ == '__main__':
-    score_train, score_test, delta_time = fit_and_test_net_on_MNIST(30, 128, 20, 0.1, 0.1)
+    score_train, score_test, delta_time = fit_and_test_net_on_MNIST(30, 128, 20, 0.1)
     print()
     print('Delta time =', delta_time)
     print('Test loss:', score_test[0])
